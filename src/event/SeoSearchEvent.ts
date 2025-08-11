a4b2e745f3b34a14418322585681370b0a53ecbe
seadonggyun4 <seadonggyun@gmail.com>
2025-08-11 10:24:21 +0900

    fix seo-select, seo-select-search event

--- src/event/SeoSearchEvent.ts
+++ src/event/SeoSearchEvent.ts
@@ -1,29 +1,20 @@
 import { EVENT_NAMES } from '../constants/constants.js';
 
-export class SeoSearchChangeEvent extends Event {
-  public readonly searchText: string;
-  public readonly previousSearchText?: string;
-
+export class SeoSearchChangeEvent extends CustomEvent<
+  { searchText: string } | { searchText: string; previousSearchText: string }
+> {
   constructor(searchText: string, previousSearchText?: string) {
-    super(EVENT_NAMES.SEARCH_CHANGE, { bubbles: true, composed: true });
-    this.searchText = searchText;
-    if(previousSearchText) this.previousSearchText = previousSearchText;
+    const detail = { searchText, ...(previousSearchText !== undefined ? { previousSearchText } : {}) };
+    super(EVENT_NAMES.SEARCH_CHANGE, { detail, bubbles: true, composed: true });
   }
 }
 
-export class SeoSearchFilterEvent extends Event {
-  public readonly filteredOptions: Array<{ value: string; label: string }>;
-  public readonly searchText: string;
-  public readonly hasResults: boolean;
-
-  constructor(
-    filteredOptions: Array<{ value: string; label: string }>,
-    searchText: string,
-    hasResults: boolean = true
-  ) {
-    super(EVENT_NAMES.SEARCH_FILTER, { bubbles: true, composed: true });
-    this.filteredOptions = filteredOptions;
-    this.searchText = searchText;
-    this.hasResults = hasResults;
+export class SeoSearchFilterEvent extends CustomEvent<{ 
+  filteredOptions: Array<{ value: string; label: string }>; 
+  searchText: string; 
+  hasResults: boolean 
+}> {
+  constructor(filteredOptions: Array<{ value: string; label: string }>, searchText: string, hasResults: boolean = true) {
+    super(EVENT_NAMES.SEARCH_FILTER, { detail: { filteredOptions, searchText, hasResults }, bubbles: true, composed: true });
   }
 }
--- src/event/SeoSelectEvent.ts
+++ src/event/SeoSelectEvent.ts
@@ -1,59 +1,33 @@
 import { EVENT_NAMES } from '../constants/constants.js';
 
-export class SeoSelectEvent extends Event {
-  public readonly label: string;
-  public readonly value: string;
+type SelectDetail = { label: string; value: string };
 
+export class SeoSelectEvent extends CustomEvent<SelectDetail> {
   constructor(eventType: string, label: string, value: string) {
-    super(eventType, { bubbles: true, composed: true });
-    this.label = label;
-    this.value = value;
+    super(eventType, { detail: { label, value }, bubbles: true, composed: true });
   }
 }
 
-export class SeoDeselectEvent extends Event {
-  public readonly label: string;
-  public readonly value: string;
-
-  constructor(label: string, value: string) {
-    super(EVENT_NAMES.DESELECT, { bubbles: true, composed: true });
-    this.label = label;
-    this.value = value;
+export class SeoDeselectEvent extends CustomEvent<SelectDetail> {
+  constructor(eventType: string, label: string, value: string) {
+    super(eventType, { detail: { label, value }, bubbles: true, composed: true });
   }
 }
 
-export class SeoResetEvent extends Event {
-  public readonly label?: string;
-  public readonly value?: string;
-  public readonly labels?: string[];
-  public readonly values?: string[];
-
-  constructor(data: {
-    label?: string;
-    value?: string;
-    labels?: string[];
-    values?: string[];
-  }) {
-    super(EVENT_NAMES.RESET, { bubbles: true, composed: true });
-    
-    if (data.label !== undefined) this.label = data.label;
-    if (data.value !== undefined) this.value = data.value;
-    if (data.labels !== undefined) this.labels = data.labels;
-    if (data.values !== undefined) this.values = data.values;
+export class SeoResetEvent extends CustomEvent<unknown> {
+  constructor(detail?: unknown) {
+    super(EVENT_NAMES.RESET, { detail, bubbles: true, composed: true });
   }
 }
 
-export class SeoChangeEvent extends Event {
+export class SeoChangeEvent extends CustomEvent<undefined> {
   constructor() {
-    super(EVENT_NAMES.CHANGE, { bubbles: true, composed: true });
+    super(EVENT_NAMES.CHANGE, { detail: undefined, bubbles: true, composed: true });
   }
 }
 
-export class SeoOpenEvent extends Event {
-  public readonly selectInstance: any;
-
-  constructor(selectInstance: any) {
-    super(EVENT_NAMES.SELECT_OPEN, { bubbles: true, composed: true });
-    this.selectInstance = selectInstance;
+export class SeoOpenEvent extends CustomEvent<{ selectInstance?: any }> {
+  constructor(selectInstance?: any) {
+    super(EVENT_NAMES.SELECT_OPEN, { detail: { selectInstance }, bubbles: true, composed: true });
   }
 }
